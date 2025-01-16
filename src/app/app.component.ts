ca369a8e7d117de73d08f6e6debfd6e4c0ab3b86
Dorian Damian <dorian.damian355@gmail.com>
2025-01-16 23:39:33 +0200

    v0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { CreateExpenseComponent } from "./create-expense/create-expense.component";
+import { ExpenseComponent } from "./expense/expense.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CreateExpenseComponent],
+  imports: [RouterOutlet, NavbarComponent, CreateExpenseComponent, ExpenseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
